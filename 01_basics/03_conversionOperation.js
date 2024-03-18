7c32c8c8393398637298e569f17c917433319d2c
SHANTANU-RAI <145670486+SHANTANU-RAI@users.noreply.github.com>
2024-03-18 14:11:29 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "33";
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof score);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+// null => 0
+// undefined => NaN
+
+let isloggedin = 1;
+let booleanisLoggedIn = Boolean(isloggedin);
+console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "shan" => true
+
+let s = 33;
+let stringNumber = String(s);
+console.log(typeof s);
